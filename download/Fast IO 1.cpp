d75e090d2169901248b48cec8d6055057574c46d
karriganasta <457612138@qq.com>
2018-05-01 17:41:47 +0800

    Site updated: 2018-05-01 17:41:41

---
+++ download/Fast IO 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline char gc(){return A==B&&(B=(A=ss)+fread(ss,1,1<<20,stdin),A==B)?EOF:*A++;}
+inline int read(void){int x=0,f=1;char c=getchar();while(!isdigit(c)){f=c=='-'?-1:1;c=gc();}while(isdigit(c)){x=(x<<3)+(x<<1)+c-'0';c=gc();}}
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
+
